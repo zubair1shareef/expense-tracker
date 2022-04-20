37b15634213653333a85100bf73825b0656e57e2
zubair1shareef <zubair1shareef@gmail.com>
2022-04-20 16:14:54 +0530

    signup

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,10 @@
 
 
 const User=require('../models/user')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
 
 exports.createUser=async(req,res)=>{
     const name=req.body.name;
@@ -14,16 +18,23 @@ exports.createUser=async(req,res)=>{
            
             res.json({msg:"user already exists"})
         }
+        pass=''
+         bcrypt.hash(password, saltRounds, function(err, hash) {
+            pass=hash
+        });
         User.create({
+          
             name:name,
             email:email,
         
             phonenumber:5695451,
-            password:password
+            password:pass
+
+           
     
         })
         .then(()=>{
-            res.json({name:req.body.name,email:req.body.email})
+            res.json({name:req.body.name,email:req.body.email,pass})
     
         })
         .catch(err=>{
